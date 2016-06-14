eec6105806353c5c5dcd65aca4ef4ef7080250e4
Valeriy Bdtaev <sg3des@gmail.com>
2016-06-14 19:19:02 +0400

    plugin

---
+++ goinstall.py
@@ -0,0 +1,12 @@
+import sublime, sublime_plugin, os
+
+class goinstall(sublime_plugin.EventListener):
+	
+	def on_post_save(self, view):
+		if view.file_name().endswith('.go') is not True:
+			return
+
+		print("goinstall %s"%view.file_name())
+
+		folder = os.path.dirname(view.file_name())
+		view.window().run_command('exec',{'cmd':['/bin/go',"install"],'working_dir':folder})
